facbf8b48278b05fb14b85f8b224211223a7c383
Paul Sessbruegger <paul.at.home@gmx.de>
2023-02-08 19:46:25 +0100

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,24 @@ module.exports = {
     './app/javascript/**/*.js'
   ]
 }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'Jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        GREEN: '#A2CBB3',
+        DARKERGREEN: '#9EBBAA',
+        BEIGE: '#F9F0E1',
+      }
+    },
+  },
+  plugins: [],
+}
