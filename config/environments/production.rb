f66d3f8b2e3b9e8f60ba356649053abc05e13fd4
Paul Sessbruegger <paul.at.home@gmx.de>
2023-02-20 13:20:05 +0100

    animation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,18 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
+
+
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
